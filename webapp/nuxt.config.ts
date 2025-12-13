81f92afbebd9a92defcd9bf5cd7e0d98cedfe09b
Quentin Neyraud <quentin.neyraud@gmail.com>
2025-12-13 15:25:48 +0100

    Setup typescript

--- webapp/nuxt.config.ts
+++ webapp/nuxt.config.ts
@@ -1,5 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: {
+    enabled: true
+  },
+
+  // Typescript
+  typescript: {
+    typeCheck: true,
+    strict: true,
+    tsConfig: {
+      compilerOptions: {
+        noUncheckedIndexedAccess: true,
+      },
+    },
+  },
 })
